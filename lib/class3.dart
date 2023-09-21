600fecd088b35020bd55e4b9a42798d34c582ff1
BappyGuria <guriabappye8@gmail.com>
2023-09-21 07:44:30 +0600

    Update

--- lib/class3.dart
+++ lib/class3.dart
@@ -10,8 +10,28 @@ class class3 extends StatefulWidget {
 class _class3State extends State<class3> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return SafeArea(
+      child: Scaffold(
+            body: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                      onPressed: (){},
+                      child: Text('Send Email',style: TextStyle(
+                        color: Colors.white
+                      ),),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.pink,
+                    ),
 
+                  ),
+                  TextButton(onPressed: (){}, child: Text('Text Buttton')),
+                  IconButton(onPressed: (){}, icon: Icon(Icons.add_call))
+                ],
+              ),
+            )
+      ),
     );
   }
 }
