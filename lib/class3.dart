e6e32ce724026bb365cc09cb4e27020af7323e5c
BappyGuria <guriabappye8@gmail.com>
2023-09-21 08:43:58 +0600

    Update

--- lib/class3.dart
+++ lib/class3.dart
@@ -1,48 +1,47 @@
 import 'package:flutter/material.dart';
 
-class class3 extends StatefulWidget {
+class class3 extends StatelessWidget {
   const class3({super.key});
 
-  @override
-  State<class3> createState() => _class3State();
-}
-
-class _class3State extends State<class3> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(
-          title: Text(
-            'GusterDecetor',
-            style: TextStyle(color: Colors.white),
-          ),
+          title: Text('List View'),
           centerTitle: true,
-          backgroundColor: Colors.red,
+          backgroundColor: Colors.blue,
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              GestureDetector(
-                onTap: () {
-                  print('Click is GestureDetector');
-                },
-                child: Text(
-                  'GestureDextector',
-                  style: TextStyle(fontSize: 32),
-                ),
-              ),
-              InkWell(
-                onTap: (){
-                  print('Click is InkWell');
-                },
-                borderRadius: BorderRadius.circular(10),
-                child: Text('InkWell',style: TextStyle(fontSize: 32),),
+        body: ListView(
+          // scrollDirection: Axis.horizontal,
+          scrollDirection: Axis.vertical,
+          children: [
+            Container(
+              width: 300,
+              height: 400,
+              color: Colors.red,
+            ),
+            Container(
+              width: 300,
+              height: 400,
+              color: Colors.blue,
+            ),Container(
+              width: 300,
+              height: 400,
+              color: Colors.yellow,
+            ),
+            Container(
+              width: 300,
+              height: 400,
+              color: Colors.grey,
+            ),
+            Container(
+              width: 300,
+              height: 400,
+              color: Colors.red,
+            ),
 
-              )
-            ],
-          ),
+          ],
         ),
       ),
     );
